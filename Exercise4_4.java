f62ed4680c8186c197f45f6a01598120cc160193
Nehir <250541039@firat.edu.tr>
2025-12-04 00:00:16 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -1,3 +1,5 @@
+//öğrenci numarası: 250541039
+// adı-soyadı: Nehir KÖKTEN
 public class Exercise4_4 {
 
     public static int getNumber() {
@@ -10,11 +12,15 @@ public class Exercise4_4 {
 
     public static void main(String[] args) {
 
-        // 1) Donus degerini kullanmayin -> ne oluyor?
-        getNumber();
+        // 1) Donus degerini kullanmayin -> ne oluyor? 
+        //Java'da geriye değer döndüren bir metot çağrıldığında, dönen veriyi yakalamak (bir değişkene atamak) zorunlu değildir. Metot çalışır, dönüş değerini üretir, fakat bu değer kullanılmadığı için hafızada işleme alınmadan yok sayılır. Bu durum sözdizimi (syntax) açısından bir hata teşkil etmez
+        getNumber(); 
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
+        //sayHello() metodu void olarak tanımlanmıştır, bu da metodun geriye herhangi bir veri tipi döndürmediği anlamına gelir. Matematiksel toplama operatörü (+) ise iki değerin toplanmasını bekler. Burada void (değersizlik) ile 7 (tamsayı) toplanmaya çalışıldığı için tür uyuşmazlığı hatası alınmıştır. Java, veri içermeyen bir yapıyı işlemlerde kullanamaz.
         // System.out.println(sayHello() + 7);
+        //Hata Mesajı: Error: operator + cannot be applied to void, int
+        
 
         // Cevaplarinizi yorum olarak ekleyin.
     }
